72e1be82852a89cce7a0781097d49a83a9a36fbe
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-02-27 17:22:26 +0000

    Updated Lib

---
+++ Praticas/TP2/MyParallelogram.js
@@ -0,0 +1,33 @@
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyParallelogram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			0, 0, 0,	//0
+			2, 0, 0,	//1
+            3, 1, 0,    //2
+            1, 1, 0     //3
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+            0, 1, 3, 
+            1, 2, 3,
+            3, 1, 0, 
+            3, 2, 1
+		];
+
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of four to draw quads
+		this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+
+		this.initGLBuffers();
+	}
+}
